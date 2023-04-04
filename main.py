c96607b0fbfac169151b4ece5b7719d7ab26f2bf
Chators <test.thibaud@protonmail.com>
2023-04-05 00:34:59 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+
+import requests
+import json
+import ipaddress
+import uuid
+import os
+from datetime import datetime
+
+if not os.path.exists('./reports'):
+    os.makedirs('./reports')
+    
+apiKey = 'REPLACE'
+fileIp = open('ip_list.txt', 'r')
+ips = fileIp.readlines()
+fileIp.close()
+
+url = 'https://api.abuseipdb.com/api/v2/check'
+ipsInformation = []
+for ip in ips:
+    ip = ip.rstrip()
+    if not ipaddress.ip_address(ip).is_private:
+        querystring = {
+            'ipAddress': ip,
+            'maxAgeInDays': '90'
+        }
+        headers = {
+            'Accept': 'application/json',
+            'Key': apiKey
+        }
+        response = requests.request(method='GET', url=url, headers=headers, params=querystring)
+        decodedResponse = json.loads(response.text)['data']
+        ipsInformation.append({
+            'ip': decodedResponse['ipAddress'],
+            'maliciousScore': decodedResponse['abuseConfidenceScore'],
+            'totalReports': decodedResponse['totalReports'],
+            'isp': decodedResponse['isp'],
+            'domain': decodedResponse['domain'],
+            'hostnames': '\n'.join(decodedResponse['hostnames']),
+            'countryCode': decodedResponse['countryCode']
+        })
+
+ipsInformation = sorted(ipsInformation, key=lambda x: x['maliciousScore'], reverse=True)
+fileOutputPath = './reports/'+datetime.now().strftime("%Y-%m-%d-%H-%M-%S!")+str(uuid.uuid4())+'.csv'
+fileOutput = open(fileOutputPath, 'w')
+fileOutput.write('IP,Malicious Score,Total Reports,ISP,Domain,Hostnames,Country Code\n')
+for ipInformation in ipsInformation:
+    fileOutput.write(f'{ipInformation["ip"]},{ipInformation["maliciousScore"]},{ipInformation["totalReports"]},{ipInformation["isp"]},{ipInformation["domain"]},"{ipInformation["hostnames"]}",{ipInformation["countryCode"]}\n')
+fileOutput.close()
+print(f'Your report is available at {fileOutputPath}')
