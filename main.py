1b4470ddcdbbc7f49d80246fdeed8961a6d10e13
Chators <test.thibaud@protonmail.com>
2023-05-02 01:34:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import requests
 import json
 import ipaddress
@@ -16,28 +15,35 @@ fileIp.close()
 
 url = 'https://api.abuseipdb.com/api/v2/check'
 ipsInformation = []
-for ip in ips:
+total_ips = len(ips)
+print(f'[!] Number of ips : {total_ips}')
+for i, ip in enumerate(ips):
+    count = i + 1
     ip = ip.rstrip()
-    if not ipaddress.ip_address(ip).is_private:
-        querystring = {
-            'ipAddress': ip,
-            'maxAgeInDays': '90'
-        }
-        headers = {
-            'Accept': 'application/json',
-            'Key': apiKey
-        }
-        response = requests.request(method='GET', url=url, headers=headers, params=querystring)
-        decodedResponse = json.loads(response.text)['data']
-        ipsInformation.append({
-            'ip': decodedResponse['ipAddress'],
-            'maliciousScore': decodedResponse['abuseConfidenceScore'],
-            'totalReports': decodedResponse['totalReports'],
-            'isp': decodedResponse['isp'],
-            'domain': decodedResponse['domain'],
-            'hostnames': '\n'.join(decodedResponse['hostnames']),
-            'countryCode': decodedResponse['countryCode']
-        })
+    print(f'[!] {count}/{total_ips} Retrieve information : {ip}')
+    try:
+        if not ipaddress.ip_address(ip).is_private:
+            querystring = {
+                'ipAddress': ip,
+                'maxAgeInDays': '90'
+            }
+            headers = {
+                'Accept': 'application/json',
+                'Key': apiKey
+            }
+            response = requests.request(method='GET', url=url, headers=headers, params=querystring)
+            decodedResponse = json.loads(response.text)['data']
+            ipsInformation.append({
+                'ip': decodedResponse['ipAddress'],
+                'maliciousScore': decodedResponse['abuseConfidenceScore'],
+                'totalReports': decodedResponse['totalReports'],
+                'isp': decodedResponse['isp'],
+                'domain': decodedResponse['domain'],
+                'hostnames': '\n'.join(decodedResponse['hostnames']),
+                'countryCode': decodedResponse['countryCode']
+            })
+    except:
+        print(f'[X] Failed to retrieve information : {ip}')
 
 ipsInformation = sorted(ipsInformation, key=lambda x: x['maliciousScore'], reverse=True)
 fileOutputPath = './reports/'+datetime.now().strftime("%Y-%m-%d-%H-%M-%S!")+str(uuid.uuid4())+'.csv'
@@ -46,4 +52,4 @@ fileOutput.write('IP,Malicious Score,Total Reports,ISP,Domain,Hostnames,Country 
 for ipInformation in ipsInformation:
     fileOutput.write(f'{ipInformation["ip"]},{ipInformation["maliciousScore"]},{ipInformation["totalReports"]},{ipInformation["isp"]},{ipInformation["domain"]},"{ipInformation["hostnames"]}",{ipInformation["countryCode"]}\n')
 fileOutput.close()
-print(f'Your report is available at {fileOutputPath}')
+print(f'[!] Your report is available at {fileOutputPath}')
